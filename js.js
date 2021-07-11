bc990fe5007a02a21bbfef217a12b775ba446154
wkndavid <workingnown@gmail.com>
2021-07-11 20:32:20 -0300

    continue calculator in js

--- js.js
+++ js.js
@@ -1,12 +1,20 @@
-function somar(num1, num2) {
-    if(typeof num1 === 'number' && typeof num2 === 'number') {
-        resultado = num1 + num2;
-        document.write(`O resultado da soma é : ${resultado} <br>`);
-    }
-    else
-    
-        document.write(`Os números fornecidos não são numéricos`);
+function insert(num){
+    var numero = document.getElementById('output').innerHTML;
+    document.getElementById('output').innerHTML=numero + num;
+} 
+function clean(){
+    document.getElementById('output').innerHTML = ''; 
+}
+function apagar1(){
+    var res = document.getElementById('output').innerHTML;
+    document.getElementById('output').innerHTML = res.substring(0, res.length -1);
+
+}
+function calcular(){
+    var res = document.getElementById('output').innerHTML;
+    if (res){
+         document.getElementById('output').innerHTML = eval(res);
+    } else {
+        document.getElementById('output').innerHTML = 'Error';
     }
 }
-somar (200, 300); 
-somar (200, '200');
